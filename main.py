72e93a01e338527b6b514eb73f23474481b73c31
harum <haru.mattsu0415@gmail.com>
2024-08-12 22:58:28 +0900

    アプリの説明のためのテキストコントロールを2つに分割

--- main.py
+++ main.py
@@ -16,12 +16,13 @@ def main(page: ft.Page):
             result_text.value = "これは回分ではありません"
         result_text.update()
 
-    text = ft.Text(value="入力された言葉が回文かどうかを判定します。\n空白は無視されます。")
+    text1 = ft.Text(value="入力された言葉が回文かどうかを判定します。")
+    text2 = ft.Text(value="空白は無視されます。")
     input_text = ft.TextField(label="ここに言葉を入力", value="", on_change=set_reversed_text)
     reversed_text = ft.TextField(label="反転した結果", value="", read_only=True)
     judge_button = ft.ElevatedButton(text="Judge!", on_click=judge)
     result_text = ft.Text(value="")
 
-    page.add(text, input_text, reversed_text, judge_button, result_text)
+    page.add(text1, text2, input_text, reversed_text, judge_button, result_text)
 
 ft.app(main)
